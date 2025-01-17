24e2cd1ed63bdc26b35623bc032e14b320ba4841
Raviv <raken100@gmail.com>
2025-01-17 19:03:42 +0200

    implemented test for TwoSum with Theory and InlineData

---
+++ LeetCodePatterns.Tests/ArraySolverTests/TwoSumSolverTests.cs
@@ -0,0 +1,17 @@
+namespace LeetCodePatterns.Tests.ArraySolverTests;
+
+public class TwoSumSolverTests
+{
+    [Theory]
+    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+    [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+    [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 }, 9, new int[] { 3, 4 })]
+    [InlineData(new int[] { 8, 5, 3, 7 }, 12, new int[] { 1, 3 })]
+    public void TwoSum_CorrectIndicesReturned(int[] nums, int target, int[] expectedIndices)
+    {
+        var solver = new TwoSumSolver();
+        var result = solver.TwoSum(nums, target);
+        Assert.Equal(expectedIndices, result);
+    }
+}

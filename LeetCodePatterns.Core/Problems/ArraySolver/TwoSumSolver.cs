be42866511f38309186ef6351257427b9a533545
Raviv <raken100@gmail.com>
2025-01-17 17:21:56 +0200

    Created basic structure for the solvers of the coding problems

---
+++ LeetCodePatterns.Core/Problems/ArraySolver/TwoSumSolver.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeetCodePatterns.Core.Problems.ArraySolver;
+
+public class TwoSumSolver
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        throw new NotImplementedException();
+    }
+}

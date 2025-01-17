d21decb5810f8a781df40b391bae5a5b17a4600e
Raviv <raken100@gmail.com>
2025-01-17 18:31:39 +0200

    implemented solution for TwoSum coding problem

--- LeetCodePatterns.Core/Problems/ArraySolver/TwoSumSolver.cs
+++ LeetCodePatterns.Core/Problems/ArraySolver/TwoSumSolver.cs
@@ -6,6 +6,16 @@ public class TwoSumSolver
 {
     public int[] TwoSum(int[] nums, int target)
     {
-        throw new NotImplementedException();
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if(dict.ContainsKey(complement))
+            {
+                return new int[] { dict[complement], i };
+            }
+            dict.Add(nums[i], i);
+        }
+        return Array.Empty<int>();
     }
 }

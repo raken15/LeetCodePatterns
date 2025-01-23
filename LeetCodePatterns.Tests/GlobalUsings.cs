dbe007b58fdeb699cc036260ef3d23f345713ae5
Raviv <raken100@gmail.com>
2025-01-23 12:33:23 +0200

    Add global using for the helpers and the patterns classes

--- LeetCodePatterns.Tests/GlobalUsings.cs
+++ LeetCodePatterns.Tests/GlobalUsings.cs
@@ -1,2 +1,4 @@
 global using LeetCodePatterns.Core.Problems.ArraySolver;
 global using LeetCodePatterns.Core.Problems.StringSolver;
+global using LeetCodePatterns.Core.Problems.CommonPatterns;
+global using LeetCodePatterns.Core.Helpers;

7317ee75a8870aa2e2a20604e1f6af594c03567d
Raviv <raken100@gmail.com>
2025-01-24 17:21:00 +0200

    Added test method for case where we have elements with equal value

--- LeetCodePatterns.Tests/CommonPatternsTests/TopKElementsTests.cs
+++ LeetCodePatterns.Tests/CommonPatternsTests/TopKElementsTests.cs
@@ -46,4 +46,15 @@ public class TopKElementsTests
 
         Assert.Equal(expected, result);
     }
+    [Theory]
+    [InlineData(new int[] { 1, 1, 2 }, 2, new int[] { 2, 1 })]
+    [InlineData(new int[] { 1, 2, 2  }, 2, new int[] { 2, 2 })]
+    [InlineData(new int[] { 2, 2, 1, 1, 3, 3 }, 3, new int[] { 3, 3, 2 })]
+    public void Solve_ReturnsKElementsDescending_HaveElementsWithEqualValue(
+    int[] inputArr, int k, int[] expected)
+    {
+        var result = TopKElementsMinHeap.Solve(inputArr, k);
+
+        Assert.Equal(expected, result);
+    }
 }
